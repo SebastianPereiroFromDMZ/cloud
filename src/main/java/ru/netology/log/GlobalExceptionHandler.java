717fa98ca1dcb712eec8b4f2ac592cb40d061e83
Kost Kuzmin <Kuzmin_konstantin@bk.ru>
2023-11-07 13:29:04 +0300

    add log classes

---
+++ src/main/java/ru/netology/log/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package ru.netology.log;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex, WebRequest request) {
+        // Создание записи лога
+        String logMessage = String.format("Exception: %s, message: %s, request: %s",
+                ex.getClass().getName(), ex.getMessage(), request.getDescription(false));
+
+        // Запись лога
+        logger.log(Level.SEVERE,logMessage, ex);
+
+
+        // Возврат ответа с описанием ошибки
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
